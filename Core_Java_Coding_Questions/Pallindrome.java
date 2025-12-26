dfc82fc01af6f630f33e3cbd3d787ad2bc8f96ba
Rahul Mandal <rahulmandal99555@gmail.com>
2025-12-26 23:46:29 +0530

    New: How to check if a string is pallindrome or not 2nd solution

--- Core_Java_Coding_Questions/Pallindrome.java
+++ Core_Java_Coding_Questions/Pallindrome.java
@@ -3,17 +3,26 @@ package Core_Java_Coding_Questions;
 //2. Write a program to find if a given integer or string is a palindrome.
 public class Pallindrome{
     public static void main(String[] args){
-        String pallindrome = "MADam";
+        String pallindrome = "Madam";
         String temp = pallindrome.toLowerCase();
-        StringBuffer stringBuffer = new StringBuffer(pallindrome);
-        String ifPallindrome = stringBuffer.reverse().toString().toLowerCase();
-        if(temp.equals(ifPallindrome)){
-            System.out.println("It is a pallindrome ");
+        // StringBuffer stringBuffer = new StringBuffer(pallindrome);
+        // String ifPallindrome = stringBuffer.reverse().toString().toLowerCase();
+        // if(temp.equals(ifPallindrome)){
+        //     System.out.println("It is a pallindrome ");
+        // }
+        // else{
+        //     System.out.println("Not a pallindrome");
+        // }
+            
+
+        //2nd method 
+        String checkPallindrome = new StringBuilder(pallindrome).reverse().toString().toLowerCase();
+        if(checkPallindrome.equals(temp)){
+            System.out.println("It is a Pallindrome");
         }
         else{
             System.out.println("Not a pallindrome");
         }
-            
 
     }
 }
