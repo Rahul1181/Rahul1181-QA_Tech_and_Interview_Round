ad7368dba6552621ef124892f7dc54e0849c5bc1
Rahul Mandal <rahulmandal99555@gmail.com>
2025-12-27 00:55:38 +0530

    Update: Reverse a string using recression

--- Core_Java_Coding_Questions/ReverseString.java
+++ Core_Java_Coding_Questions/ReverseString.java
@@ -4,16 +4,31 @@ package Core_Java_Coding_Questions;
 public class ReverseString {
     public static void main(String args[]){
          String name ="Vegabond";
-        int stringLen = name.length();
-        int index = 0; 
-        char[] nameArray = new char[stringLen];
-        for(int i =stringLen -1;i >= 0 ;i--){
-            nameArray[index++] = name.charAt(i);
-        }
-        System.out.println(new String(nameArray));
+        // int stringLen = name.length();
+        // int index = 0; 
+        // char[] nameArray = new char[stringLen];
+        // for(int i =stringLen -1;i >= 0 ;i--){
+        //     nameArray[index++] = name.charAt(i);
+        // }
+        // System.out.println(new String(nameArray));
 
+
+        //2nd method to reverse a String
         // StringBuffer stringBuffer = new StringBuffer(name);
         // System.out.println(stringBuffer.reverse().toString());
+
+
+        //3rd method (Recursion)
+        System.out.println("String Reverse is " + reverseStr(name));
+    }
+    //3rd method to reverse a String (Recursion)
+    public static String reverseStr(String fullname){
+        //Edge cases
+        if(fullname.isEmpty() || fullname.length() == 1){
+            return fullname;
+        }
+        //Everytime function is called it will capture 1 element and add it with 0th element and the string is reducing at every iteration
+        return reverseStr(fullname.substring(1))+ fullname.charAt(0);
     }
     
 }
