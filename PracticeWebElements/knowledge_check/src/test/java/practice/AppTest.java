2d1f65a96dbbca86225097e441db721cee050e54
Rahul Mandal <rahulmandal99555@gmail.com>
2026-01-02 17:05:19 +0530

    Update: how to find dynamic Elements in snapdeal

---
+++ PracticeWebElements/knowledge_check/src/test/java/practice/AppTest.java
@@ -0,0 +1,53 @@
+package practice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AppTest {
+    public static WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.snapdeal.com/");
+
+    }
+
+    @Test
+    public void snapdeal() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        //driver.findElement(By.xpath("//div[text()=\"Women's Fashion\"]"));
+        WebElement womensFashion = (WebElement) js.executeScript(
+                "return [...document.querySelectorAll('div')]" +
+                        ".find(el => el.textContent.trim() === \"Women's Fashion\");");
+        js.executeScript(
+                "arguments[0].dispatchEvent(new MouseEvent('mouseover', { bubbles: true }));",
+                womensFashion);
+        js.executeScript(
+                "document.body.addEventListener(" +
+                        "'mouseout', e => e.stopPropagation(), true);");
+        driver.manage().timeouts()
+                .implicitlyWait(Duration.ofSeconds(10));
+        WebElement subMenuItem = driver.findElement(By.xpath("//div[contains(text(),'Kurtas & Kurtis')]"));
+
+        subMenuItem.click();
+
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
---
+++ PracticeWebElements/knowledge_check/src/test/java/practice/ExplicitWait.java
@@ -0,0 +1,18 @@
+package practice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExplicitWait {
+    public static WebDriver driver;
+    public void waitForElement(){
+        //  WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        //  WebElement welcomeText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("")));
+    }
+    
+}
