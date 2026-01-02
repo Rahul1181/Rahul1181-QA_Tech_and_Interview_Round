ff1b86c902335360e650192b550fe50b5a059053
Rahul Mandal <rahulmandal99555@gmail.com>
2026-01-02 20:14:34 +0530

    Update: WaitUtils.java class for explicit wait

---
+++ PracticeWebElements/knowledge_check/src/test/java/practice/WaitUtils.java
@@ -0,0 +1,40 @@
+package practice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WaitUtils {
+
+    public static WebDriver driver;
+    WebDriverWait wait;
+
+    public WaitUtils(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    // Wait for element to be clickable
+    public WebElement waitForElementToBeClickable(By locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    // Wait for visibility of element
+    public WebElement waitForVisibilityOfElement(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    // Wait for page load completion
+    public void waitForPageLoad() {
+        wait.until(driver ->
+                ((org.openqa.selenium.JavascriptExecutor) driver)
+                        .executeScript("return document.readyState")
+                        .equals("complete")
+        );
+    }
+
+}
