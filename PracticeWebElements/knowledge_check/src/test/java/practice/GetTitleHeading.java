2faef9e9d9f8d6430310513e4c1453252f47b705
Rahul Mandal <rahulmandal99555@gmail.com>
2026-01-03 23:55:23 +0530

    New: How to fetch the Heading and title of a website

---
+++ PracticeWebElements/knowledge_check/src/test/java/practice/GetTitleHeading.java
@@ -0,0 +1,52 @@
+package practice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GetTitleHeading {
+    private static WebDriver driver;
+    @BeforeClass
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/?utm_source=chatgpt.com");
+
+    }
+    //Check the heading
+    @Test
+    public void checkHeading(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement heading = driver.findElement(By.xpath("//h1[@class='heading']"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[@class='heading']")));
+        
+        String getHeading = heading.getText();
+        System.out.println("WebSite heading: "+ getHeading);
+        Assert.assertEquals(getHeading, "Welcome to the-internet", "Heading mismatch");
+
+    }
+    //check the Website Title
+    @Test
+    public void checkTitle(){
+        String title = driver.getTitle();
+        Assert.assertEquals(title, "The Internet", "Title mismatch");
+        System.out.println("Title of the website: " + title );
+
+    }
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+    
+}
